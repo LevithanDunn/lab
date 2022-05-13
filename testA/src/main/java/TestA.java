5d5470b38d66f6ea6f9b073974b3d779b34b0e52
LevithanDunn <56142605+LevithanDunn@users.noreply.github.com>
2022-05-13 23:52:17 +0800

    Update TestA.java
    作为测试

--- testA/src/main/java/TestA.java
+++ testA/src/main/java/TestA.java
@@ -11,6 +11,8 @@ public class TestA {
         //方法2：从Hutool-4.1.14开始提供
         String id2 = IdUtil.objectId();
         ArrayList k = new ArrayList();
+        //kittta
+        int x1 = 100;
         //进行开始反击
         int x = 1;
         int xk = 1000;
