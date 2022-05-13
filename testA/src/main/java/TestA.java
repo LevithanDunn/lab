442caa1462576767a2e1f085b1877a015e648824
LevithanDunn <56142605+LevithanDunn@users.noreply.github.com>
2022-05-13 19:13:52 +0800

    update .gitignore

---
+++ testA/src/main/java/TestA.java
@@ -0,0 +1,14 @@
+import cn.hutool.core.util.IdUtil;
+
+/**
+ * @author dunn
+ * @create 2022-05-13 18:40
+ */
+public class TestA {
+    public static void main(String[] args) {
+        //方法2：从Hutool-4.1.14开始提供
+        String id2 = IdUtil.objectId();
+        System.out.println("id2 = " + id2);
+        System.out.println("Aaaa");
+    }
+}
