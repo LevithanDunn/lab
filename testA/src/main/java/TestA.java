1cc8c0225b5d290a487a77ba6b5df9d5ae588e13
LevithanDunn <56142605+LevithanDunn@users.noreply.github.com>
2022-05-14 00:24:38 +0800

    实现ignorexk

---
+++ testA/src/main/java/TestA.java
@@ -0,0 +1,22 @@
+import cn.hutool.core.util.IdUtil;
+
+import java.util.ArrayList;
+
+/**
+ * @author dunn
+ * @create 2022-05-13 18:40
+ */
+public class TestA {
+    public static void main(String[] args) {
+        //方法2：从Hutool-4.1.14开始提供
+        String id2 = IdUtil.objectId();
+        ArrayList k = new ArrayList();
+        //kittta
+        int x1 = 100;
+        //进行开始反击
+        int x = 1;
+        int xk = 1000;
+        System.out.println("id2 = " + id2);
+        System.out.println("Aaaa");
+    }
+}
